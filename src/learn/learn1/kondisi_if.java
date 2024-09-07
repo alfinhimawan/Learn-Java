d353635149bb8ce0a4b1c8880ca9e9d62f9e44fa
alfinhimawan <alfinhimawan99@gmail.com>
2024-09-07 17:10:10 +0700

    first commit

---
+++ src/learn/learn1/kondisi_if.java
@@ -0,0 +1,12 @@
+package learn.learn1;
+
+public class kondisi_if {
+    public static void main(String[] args) {
+        int nilai = 75;
+
+        // Kondisi if
+        if (nilai > 70) {
+            System.out.println("Nilai Anda di atas rata-rata");
+        }
+    }
+}
